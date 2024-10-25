f8863154a4981ed5ef3798f9871cd85a145423df
HiramDev <hiramdev02@gmail.com>
2024-10-25 17:07:01 -0600

    creando rutas

--- index.js
+++ index.js
@@ -2,5 +2,15 @@ import express from 'express'
 
 const app = express();
 
+app.get('/autores', (req, res) => res.send('obteniendo autores'));
+
+app.post('/autores', (req, res) => res.send('creando autores'));
+
+app.put('/autores', (req, res) => res.send('actualizando autores'));
+
+app.delete('/autores', (req, res) => res.send('eliminando autores'));
+
+
+
 app.listen(3000);
 console.log("Server running on port 3000")
