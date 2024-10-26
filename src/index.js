fcc9a9a40758e5c20748fd58b6a8bf4b8956d626
HiramDev <hiramdev02@gmail.com>
2024-10-26 17:45:22 -0600

    Cambiar notFound por json

--- src/index.js
+++ src/index.js
@@ -9,5 +9,11 @@ app.use(express.json())
 app.use(indexRoutes)
 app.use('/api', autoresRoutes)
 
+app.use((req, res, next) => {
+    res.status(404).json({
+        message: 'endpoint Not Found'
+    })
+})
+
 app.listen(3000);
 console.log("Server running on port 3000")
